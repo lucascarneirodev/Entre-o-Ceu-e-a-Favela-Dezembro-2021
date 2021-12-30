9d276682be82a2d23e226d38b18d7ed73fbb0579
Lucas Carneiro <60579550+lucascarneiro-dev@users.noreply.github.com>
2021-12-29 21:29:37 -0300

    style: hover transitions and parallax set

--- src/script/script.js
+++ src/script/script.js
@@ -6,6 +6,20 @@ var userFeed = new Instafeed({
 });
 userFeed.run();
 
+//new CubeGallery('instafeed-container')
 
+const parallax_home = document.getElementById("quemsomos");
 
-new CubeGallery('instafeed-container')
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    parallax_home.style.backgroundPositionY= offset * 0.5 + "px"
+})
+
+const parallax_about = document.getElementById("sobrenos");
+
+window.addEventListener("scroll", function()
+{
+    let offset_about = window.pageYOffset;
+    parallax_about.style.backgroundPositionY= offset_about * 0.5 + "px";
+})
