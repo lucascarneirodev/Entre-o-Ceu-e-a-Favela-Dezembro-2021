df26cc093666c80ee8a9dac204ea3786f4f236da
LucasCarneiroOSF <97185022+LucasCarneiroOSF@users.noreply.github.com>
2022-02-05 00:22:36 -0300

    Revert "feat: fixed backgrounds added"
    This reverts commit 242d436d4d0bc43d2936b5e40c646524705f72b5.

---
+++ src/script/script.js
@@ -0,0 +1,25 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJWSV9iSXVLV2c4bkhRZAFgtRkhFamFlY2xBbThaU2NEODNacWFlbVVGaEJRNGJBcDdjcXJUWXZA4ZA3UyUmJQSDVJVDltWjEzWnlwYTU1aHB4T0JuN3k0NVA4cWxOWTNobUhFcWtkaXE3TUNkX2EwWAZDZD'
+});
+userFeed.run();
+
+//new CubeGallery('instafeed-container')
+
+const parallax_home = document.getElementById("quemsomos");
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    parallax_home.style.backgroundPositionY= offset * 0.0 + "px"
+})
+
+const parallax_about = document.getElementById("sobrenos");
+
+window.addEventListener("scroll", function()
+{
+    let offset_about = window.pageYOffset;
+    parallax_about.style.backgroundPositionY= offset_about * 0.5 + "px";
+})
